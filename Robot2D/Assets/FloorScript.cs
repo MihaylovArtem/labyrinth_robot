f4b16cd60c9cc69270831e575978e87f7af82e6c
okeyka <okeyka74@gmail.com>
2016-11-28 21:14:05 +0500

    initial commit

---
+++ Robot2D/Assets/FloorScript.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.IO;
+using UnityEngine;
+using System.Collections;
+using UnityEngine.Assertions.Must;
+
+public class FloorScript : MonoBehaviour {
+    public GameObject Wall;
+    public int N;
+    public int M;
+    void Start() {
+	    TextReader reader = File.OpenText("map.txt");
+	        var line = reader.ReadLine();
+	        Console.WriteLine(line);
+	        var bits = line.Split(' ');
+	        N = int.Parse(bits[0]);
+	        M = int.Parse(bits[1]);
+            gameObject.transform.localScale = new Vector2(N,M);
+	        gameObject.transform.position = new Vector2(N/2f, M/2f);
+	        //Camera.main.transform.position = new Vector3(N/2f, 10f, M/2f);
+	        //Camera.main.fieldOfView = Math.Max(N, M)*4/3f; 
+	        for (var i = 0; i < N; i++)
+	        {
+	            line = reader.ReadLine();
+	            bits = line.Split(' ');
+	            for (var j = 0; j < M; j++)
+	            {
+	                var coor = int.Parse(bits[j]);
+	                if (coor == 0) continue;
+                    GameObject wall = Instantiate(Wall);
+	                wall.transform.position = new Vector2(j + 0.5f, i + 0.5f);
+	            }
+	        }
+	}
+
+    void Update() {
+
+    }
+}
---
+++ Robot2D/Assets/PlayerScript.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerScript : MonoBehaviour {
+    public const int maxSpeed = 10;
+    public GameObject leftWheel;
+    public GameObject rightWheel;
+    [Range(-100, 100)]
+    public int LeftPower;
+    [Range(-100, 100)]
+    public int RightPower;
+
+    private Rigidbody2D leftWheelRigidBody;
+    private Rigidbody2D rightWheelRigidBody;
+    private Rigidbody2D thisRigidBody;
+
+    // Use this for initialization
+    void Start() {
+        leftWheelRigidBody = leftWheel.GetComponent<Rigidbody2D>();
+        rightWheelRigidBody = rightWheel.GetComponent<Rigidbody2D>();
+        thisRigidBody = gameObject.GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate() {
+        leftWheelRigidBody.velocity = leftWheel.transform.up * maxSpeed * LeftPower / 100;
+        rightWheelRigidBody.velocity = rightWheel.transform.up * maxSpeed * RightPower / 100;
+    }
+}
